b3f64261e4affa4a3181b8152548f64f42233088
Morgan Massamba <chabcontact@gmail.com>
2022-04-01 21:34:55 +0200

    wip

--- server.js
+++ server.js
@@ -10,15 +10,6 @@ const exercisesRoutes = require('./routes/exercises');
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    const sql = 'SELECT * FROM users;';
-    db.query(sql, (error, results) => {
-        if (error) throw error;
-
-        res.json({ data: results }).status(200);
-    });
-});
-
 app.use('/auth', authRoutes);
 app.use('/exercises', exercisesRoutes);
 
